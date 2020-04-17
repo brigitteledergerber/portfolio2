a1991d39bf5ff0afa3c6cb90b980d61fd1f49af2
brigitteledergerber <57382999+brigitteledergerber@users.noreply.github.com>
2020-04-17 13:13:11 -0700

    Added all files from Replit portfolio

---
+++ index.php
@@ -0,0 +1,18 @@
+<html> <!--contains all html code-->
+
+  <head> <!--identifies header section to separate information that will not be shown in user interface-->
+    <title>PHP Test</title> <!--defines the page's title that is shown in the page's tab-->
+  </head> <!--closing tag-->
+
+  <body> <!--identifies body section that encloses elements displayed on the web page-->
+    <?php echo '<p>Hello World! Here are all of my Links [Brigitte Ledergerber Period 2]</p>'; ?> <!--text displayed on screen-->
+    <ul> <!--creates an unordered list-->
+      <li><a href="index.html">index.html</a></li></br>
+      <!--list item of a hyperlink to Home Page added to text-->
+      <li><a href="Ledergerber_AboutMe.html">Ledergerber_AboutMe.html</a></li></br> <!--list item of a hyperlink to About Me Page added to text-->
+      <li><a href="Ledergerber_Projects.html">Ledergerber_Projects.html</a></li></br> <!--list item of a hyperlink to Projects Page added to text-->
+      <li><a href="Ledergerber_Resume.html">Ledergerber_Resume.html</a></li></br> <!--list element of a hyperlink to Work Experience Page added to text-->
+      <li><a href="Ledergerber_ContactMe.html">Ledergerber_ContactMe.html</a></li></br> <!--list element of a hyperlink to Contact Me Page added to text-->
+    <ul> <!--closing tag-->
+  </body> <!--closing tag-->
+</html> <!--closing tag-->
